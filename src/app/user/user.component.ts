7f3491fb636ae48ae91b711c1ea1e48426daf101
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-28 15:58:03 +0700

    output function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +7,14 @@ import { Component, computed, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  id = input.required<string>();
   avatar = input.required<string>();
   name = input.required<string>();
+  select = output<string>();
 
   imagePath = computed(() => `assets/users/${this.avatar()}`);
 
-  onSelectUser(): void {}
+  onSelectUser(): void {
+    this.select.emit(this.id());
+  }
 }
