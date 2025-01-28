4f9ae57ba079cc0d08cd27d37f941140d13a1e40
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-28 16:23:51 +0700

    add tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'task-app';
   users = DUMMY_USERS;
+  selectedUserId = signal('u1');
 
-  onSelectUser(id: string) {}
+  selectedUser = computed(
+    () => this.users.find((user) => user.id === this.selectedUserId())!
+  );
+
+  onSelectUser(id: string) {
+    this.selectedUserId.set(id);
+  }
 }
