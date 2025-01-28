04fc0849e93dccf511f62e4fe04c885f4817ae2d
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-28 16:11:31 +0700

    task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'task-app';
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
-  onSelectUser(id: string) {}
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
