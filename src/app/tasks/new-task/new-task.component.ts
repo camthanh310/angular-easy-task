f327e275c466c1d979a1bbb08d7a1c4611f348ba
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-29 20:46:56 +0700

    2way binding signal

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,17 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   cancel = output<void>();
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDate = signal('');
 
   onCancel(): void {
     this.cancel.emit();
