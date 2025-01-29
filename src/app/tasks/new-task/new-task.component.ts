12057df37d9d91c10d7acbe2cae8a06b8e5eaa48
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-29 21:10:41 +0700

    update task with signal

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -9,11 +10,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   cancel = output<void>();
+  add = output<NewTaskData>();
   enteredTitle = signal('');
   enteredSummary = signal('');
   enteredDate = signal('');
 
   onCancel(): void {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle(),
+      summary: this.enteredSummary(),
+      date: this.enteredDate(),
+    });
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -52,6 +53,20 @@ export class TasksComponent {
   }
 
   onCancelAddTask() {
+    this.isAddingTask.set(false);
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    const newTask = {
+      id: new Date().getTime().toString(),
+      userId: this.userId(),
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    };
+
+    this.tasks.update((values) => [newTask, ...values]);
+
     this.isAddingTask.set(false);
   }
 }
