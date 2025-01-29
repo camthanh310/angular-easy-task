c00ebe4433f45bd11078cecd401754b0c1d4db4e
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-01-29 08:18:06 +0700

    separate file interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, computed, input, output } from '@angular/core';
-
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
