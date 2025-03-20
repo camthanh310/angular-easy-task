4c16a1c58746bfca339fa809db5251f9845510a8
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-03-20 21:23:43 +0700

    fresh app with route

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from '../shared/card/card.component';
+import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
@@ -10,14 +10,8 @@ import { CardComponent } from '../shared/card/card.component';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-  @Input({ required: true }) selected!: boolean;
-  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return `assets/users/${this.user.avatar}`;
-  }
-
-  onSelectUser(): void {
-    this.select.emit(this.user.id);
   }
 }
