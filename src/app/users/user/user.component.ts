277c7a0f78699e2122ad738b68486249533fda6f
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-03-20 21:31:14 +0700

    remove app card

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from '../../shared/card/card.component';
 
 @Component({
   selector: 'app-user',
-  imports: [CardComponent],
+  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
