e2e7d190be5374a64f5b29a9214ee9cc621717be
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2025-03-24 20:46:45 +0700

    title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
+  resolveTitle,
   resolveUserName,
   UserTasksComponent,
 } from './users/user-tasks/user-tasks.component';
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
+    title: 'No task selected',
   },
   {
     path: 'users/:userId',
@@ -22,6 +24,7 @@ export const routes: Routes = [
     resolve: {
       userName: resolveUserName,
     },
+    title: resolveTitle,
   },
   {
     path: '**',
